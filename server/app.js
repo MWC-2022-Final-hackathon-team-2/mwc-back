5c71eef2be663dc25bd09e3165f6dfd237382304
Daniel Españadero <developer@daniel-espanadero.com>
2022-03-01 12:49:31 +0100

    update find by id

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
-require('./src/middlewares/cors')(app);
 const { dbConection } = require('./src/db/config');
 require('dotenv').config();
 const router = require('./src/routes/company');
 const app = express();
+require('./src/middlewares/cors')(app);
 const port = process.env.PORT || 5000;
 
 app.use(express.json())
