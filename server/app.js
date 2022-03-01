04381c0e3b065864831a2654b50d67d3fe7368d7
Daniel Españadero <developer@daniel-espanadero.com>
2022-03-01 12:16:31 +0100

    cors added

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const { dbConection } = require('./src/db/config');
 require('dotenv').config();
 const router = require('./src/routes/company');
 const app = express();
 const port = process.env.PORT || 5000;
 
-
+app.use(cors())
 app.use(express.json())
 app.use('/api/v1', router)
 
