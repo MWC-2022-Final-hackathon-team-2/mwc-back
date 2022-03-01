f97716cf5069dd58569ea1002dcad88a2193ef96
Erick Ayala <erickayalrias@gmail.com>
2022-03-01 13:20:18 +0100

    aas

--- server/app.js
+++ server/app.js
@@ -4,11 +4,7 @@ const { dbConection } = require('./src/db/config');
 require('dotenv').config();
 const router = require('./src/routes/company');
 const app = express();
-<<<<<<< HEAD
-app.use(corst())
-=======
 require('./src/middlewares/cors')(app);
->>>>>>> 5c71eef2be663dc25bd09e3165f6dfd237382304
 const port = process.env.PORT || 5000;
 
 app.use(express.json())
