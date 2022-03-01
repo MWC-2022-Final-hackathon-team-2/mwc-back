8be52c7d552995e1b46dfaa32744d2ffa1726abc
Aliciaegea <aliegea4@gmail.com>
2022-03-01 14:41:27 +0100

    k

--- nuwe-front/src/pages/home/Home.js
+++ nuwe-front/src/pages/home/Home.js
@@ -1,12 +1,11 @@
-import React from "react";
-import JsGrid from "../../components/JsGrid/JsGrid";
+import React from 'react'
 
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+        <h1>Home</h1>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
