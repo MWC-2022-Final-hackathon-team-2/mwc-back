207a32ee73aacf715d60298983956e5ca0a974c1
Garikoitz Herrero Vargas <gariherrero@gmail.com>
2022-03-01 14:51:23 +0100

    update

--- nuwe-front/src/pages/home/Home.js
+++ nuwe-front/src/pages/home/Home.js
@@ -1,13 +1,10 @@
 import React from 'react'
-import AgGrid from '../../components/ag-Grid/Ag-Grid'
-import JsGrid from '../../components/JsGrid/JsGrid'
 
 const Home = () => {
   return (
     <div>
-        <AgGrid/>
+        <h1>Home</h1>
     </div>
-    
   )
 }
 
